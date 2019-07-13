71719aa8104b53618f77c4ef1a889e2f2dd3c521
Neuromancer <neuromancer43@gmail.com>
2019-07-13 18:21:32 +0800

    add random generated graphs

--- random_graph_generator.py
+++ random_graph_generator.py
@@ -20,22 +20,47 @@ def graph_plot(graph):
     plt.scatter(X,Y)
     plt.show()
 
+def graphs_decipher(target_file,n=6):
+    '''
+
+    '''
+    data=np.loadtxt(target_file)
+    N=data.shape[0]//n
+    graphs=[]
+    for i in range(N):
+        graphs.append(data[i*n:(i+1)*n])
+    return graphs
+
 '''
 use this script to generate N random graphs with n nodes
-target file is named as 'random_graph.csv', saved in current directory
+
+the graphs are vertically stacked into one big 2-D array
+
+you can extract the graph as a list from the saved file using graph_decipher
+
+saved in target file named as 'random_graph.csv' in current directory
+
+
 '''
 
 if __name__=='__main__':
     n=6 # node number of graph
-    N=2 # number of graph generated
+    N=20 # number of graph generated
+    test=True
     gen_graphs=[]
+    print("generating graphs...")
+    
     for i in range(N):
         graph=gen_graph(n)
-        graph_plot(graph)
         gen_graphs.append(graph)
+    print("finished!")
 
-    gen_graphs=np.array(gen_graphs)
-    
-    # print(gen_graphs)
-
+    gen_graphs=np.vstack(gen_graphs)
     np.savetxt('random_graphs.csv',gen_graphs)
+    print("graphs saved!")
+    
+    if test:
+        target_file="random_graphs.csv"
+        graph_plot(graphs_decipher(target_file)[0])
+        print("successfully extract graphs!")
+    
