ecf5ffd352f7ad497de9a20b2e6739378b8b08ac
Neuromancer43 <neuromancer43@gmail.com>
2019-08-23 01:13:36 +0800

    move note

--- QAOA_TSP.py
+++ QAOA_TSP.py
@@ -10,65 +10,65 @@ from lib.utilities import timer, report
 
 # %% initialize engine
 
-from lib.projectq_header import * # eng is initialized!
+from lib.projectq_header import *  # eng is initialized!
 
 print('compiler engine initialization...')
 
-postfix="_n=4_N=100"
-matrix_file="./data/random_tsp_matrix"+postfix
-solution_file="./data/qaoa_tsp_solution"
-
+postfix = "_n=4_N=100"
+matrix_file = "./data/random_tsp_matrix"+postfix
+solution_file = "./data/qaoa_tsp_solution"
 
 
 #%% set parameters
 
-p=1
-n=4
-n_qubits=n**2
-N=20
-report_rate=1
-n_sampling=50
+p = 1
+n = 4
+n_qubits = n**2
+N = 20
+report_rate = 1
+n_sampling = 50
 
-data=decode_matrix_list(matrix_file,n)
-assert N<=len(data)
-data=data[:N]
+data = decode_matrix_list(matrix_file, n)
+assert N <= len(data)
+data = data[:N]
 
 
-opt_method="COBYLA"
-opt_option={'maxiter': 100, 'tol': 0.1}
+opt_method = "COBYLA"
+opt_option = {'maxiter': 100, 'tol': 0.1}
 
-tag="_n={0}_N={1}_p={2}_method={3}".format(n,N,p,opt_method)
+tag = "_n={0}_N={1}_p={2}_method={3}".format(n, N, p, opt_method)
 
 # %%
 print("tsp with "+tag)
 
 
-solution=[]
-with open(solution_file, "w") as f: # write in time!
+solution = []
+with open(solution_file, "w") as f:  # write in time!
     for i in range(N):
-        matr=data[i]
-        H_cost=TSP_H_cost(matr)
-        H_mixer=TSP_H_mixers(n)
-        cost_func=lambda x: tsp_cost(tsp_bits_convert(x),matr)
+        matr = data[i]
+        H_cost = TSP_H_cost(matr)
+        H_mixer = TSP_H_mixers(n)
+        def cost_func(x): return tsp_cost(tsp_bits_convert(x), matr)
         # qaoa=QAOA(eng,H_cost,n_qubits,n_steps=p,H_mixer=H_mixer,ansatz_func=TSP_Ansatz,n_sampling=0,cost_eval=cost_func,verbose=True)# apply operator ansatz
 
-        qaoa=QAOA(eng,H_cost,n_qubits,n_steps=p,n_sampling=0) #naive version
+        qaoa = QAOA(eng, H_cost, n_qubits, n_steps=p,
+                    n_sampling=0)  # naive version
 
-        qaoa.run(method=opt_method,options=opt_option)
-        
-        param=qaoa.result.x
-        evaluate_cost=qaoa.result.fun
-        
-        n_iter=qaoa.result.nfev
-        conf, cost=qaoa.get_solution(draw=20)
-        
-        
+        qaoa.run(method=opt_method, options=opt_option)
+
+        param = qaoa.result.x
+        evaluate_cost = qaoa.result.fun
+
+        n_iter = qaoa.result.nfev
+        conf, cost = qaoa.get_solution(draw=20)
+
         solution.append([param, conf, cost, evaluate_cost, n_iter])
         f.write(str(solution[-1])+"\n")
-        report(i,report_rate,N)
-    
+        report(i, report_rate, N)
+
 #%%
 print("saving data...")
-pd.DataFrame(solution,columns=["opt_param", "final_state", "cost", "mean_cost", "n_iteration"]).to_csv(solution_file+tag)
+pd.DataFrame(solution, columns=["opt_param", "final_state",
+                                "cost", "mean_cost", "n_iteration"]).to_csv(solution_file+tag)
 
 #%%
