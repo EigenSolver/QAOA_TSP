1c9e6a02050d08c056f6be0972ada1aa495e15f1
Neuromancer <neuromancer43@gmail.com>
2019-09-21 12:30:38 +0800

    update circuit data

--- TSP_circuit.py
+++ TSP_circuit.py
@@ -1,39 +1,43 @@
 # -*- coding: utf-8 -*-
-from qaoa.tests.random_graph_generator import decode_matrix_list
-from qaoa.hamiltonians.graph_converter import TSP_H_cost
-from qaoa.ansatzes.operator_ansatz import TSP_H_mixers, TSP_Ansatz
-from qaoa.tests.tsp_solver import tsp_cost, tsp_bits_convert
-from qaoa.backends.qaoa import QAOA
+import os
+import sys
+from optimizeq.utils.random_graph_generator import decode_matrix_list
+from optimizeq.hamiltonians import TSP_H_cost
+from optimizeq.ansatzes import TSP_H_mixers, TSP_Ansatz
+from optimizeq.utils.tsp_solver import tsp_cost, tsp_bits_convert
+from optimizeq.utils.random_graph_generator import gen_random_points, get_distance_matrix
+from optimizeq import QAOA
+
 from projectq.ops import X, Y, Z, Rx, Ry, Rz, H, TimeEvolution, All, Measure, QubitOperator, CNOT, Swap
 from projectq.backends import Simulator, CommandPrinter, ResourceCounter
 from projectq.setups.restrictedgateset import get_engine_list
 from projectq import MainEngine
 
+import numpy as np 
+from optimizeq.utils import report
+
 engine_list = get_engine_list(
     one_qubit_gates=(X, Y, Z, Rx, Ry, Rz, H), two_qubit_gates=(CNOT,))
 eng = MainEngine(CommandPrinter(accept_input=False), engine_list)
-
-
-postfix = "_n=4_N=100"
-matrix_file = "./data/random_tsp_matrix"+postfix
-solution_file = "./data/qaoa_tsp_solution"
-
-p = 1
-n = 4
-n_qubits = (n-1)**2
-N = 1
-report_rate = 1
-n_sampling = 50
-data = decode_matrix_list(matrix_file, n)
-
 # %%
-matr = data[0]
-H_mixer = TSP_H_mixers(n-1)
-H_cost = TSP_H_cost(matr)
-main = QAOA(eng, H_cost, n_qubits, n_steps=p, H_mixer=H_mixer,
-            ansatz_func=TSP_Ansatz)  # apply operator ansatz
 
-state = main.prep_state([1, 2])
-All(Measure) | state
 
-#%%
+def print_tsp_circuit(eng, n, p):
+    n_qubits = (n-1)**2
+    sys.stdout = open("TSP_circuit_n={0}_p={1}.txt".format(n, p), "w")
+    matr = get_distance_matrix(gen_random_points(n))
+    H_mixer = TSP_H_mixers(n-1)
+    H_cost = TSP_H_cost(matr)
+    main = QAOA(eng, H_cost, n_qubits, n_steps=p, H_mixer=H_mixer,
+                ansatz_func=TSP_Ansatz)  # apply operator ansatz
+
+    state = main.prep_state(np.random.rand(2*n_qubits))
+    eng.flush()
+    All(Measure) | state
+    del state
+
+
+os.chdir("./data/circuits")
+for n in range(12, 21):
+    for p in range(1, 3):
+        print_tsp_circuit(eng, n, p)
---
+++ measure_circuit.py
@@ -0,0 +1,2 @@
+n = 1
+file_name = "TSP_circuit_n={0}_p={1}.txt".format(n,p)
